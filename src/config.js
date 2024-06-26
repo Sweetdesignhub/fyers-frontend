f1fd286b3259686b0d9c9a9a7a057e4510fae9de
Arshdeep Singh <singharshdeep9039@gmail.com>
2024-06-26 19:14:22 +0530

    doing trial

--- src/config.js
+++ src/config.js
@@ -6,7 +6,7 @@ import axios from 'axios';
   
 
 const api = axios.create({
-  baseURL: 'https://fyers-backend.onrender.com',
+  baseURL: 'http://localhost:8080',
   headers: {
     'Content-Type': 'application/json',
   },
